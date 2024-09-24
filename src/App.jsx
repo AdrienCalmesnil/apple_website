37b684890fe5dacdcd9782794a71d3bd386606e0
Adrien Calmesnil <adcalmesnil@gmail.com>
2024-09-24 21:32:23 +0200

    [Hero] video display + cta btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Highlights from "./components/Highlights"
-
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
 
 const App = () => {
   return (
-  <main className="bg-black">
-    <Navbar />
-    <Hero />
-    <Highlights/>
-    </main>);
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
 };
 
 export default App;
